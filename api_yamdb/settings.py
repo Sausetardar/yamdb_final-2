9942b62c60ffd155e2e3efc6961fd5558cd429bc
sorochinskii <sorochinsky.alex@gmail.com>
2021-06-02 11:03:19 +0500

    dockerfile fixes.

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -106,9 +106,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'files', 'static')
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
