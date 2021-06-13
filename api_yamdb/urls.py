98ee610992b43f5151406a5b7e6aaa508e8381b3
sorochinskii <sorochinsky.alex@gmail.com>
2021-06-13 15:33:54 +0500

    Redirect from root to redoc.

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', lambda red: redirect('redoc/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('redoc/', TemplateView.as_view(template_name='redoc.html'),
